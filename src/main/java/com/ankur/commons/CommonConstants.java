77ca8203814e71e8b7227f6c1f20ab59d54db4b6
Ankur Srivastava <ankurprakash.srivastava@gmail.com>
2016-06-26 17:11:11 -0700

    updated

---
+++ src/main/java/com/ankur/commons/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.ankur.commons;
+
+/**
+ * Created by ankur on 6/26/16.
+ */
+public class CommonConstants {
+    public static String baseURL="https://portland.craigslist.org";
+}
--- src/test/java/com/ankur/cl/Steps.java
+++ src/test/java/com/ankur/cl/Steps.java
@@ -1,14 +1,15 @@
 package com.ankur.cl;
 
+import com.ankur.commons.CommonConstants;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import sun.security.util.PendingException;
 
 /**
+ * Steps File To Perform Actions
  * Created by ankur on 6/26/16.
  */
 public class Steps {
@@ -16,9 +17,10 @@ public class Steps {
     @Given("^I am able to open criagslist rss$")
     public void i_am_able_to_open_criagslist_rss() throws Throwable {
         Client client=Client.create();
-        WebResource webResource=client.resource("https://portland.craigslist.org/search/cto?auto_title_status=1&amp;format=rss&amp;max_auto_miles=100000&amp;max_price=10000&amp;postal=97006&amp;search_distance=50");
+        WebResource webResource=client.resource(CommonConstants.baseURL+"/search/cto?auto_title_status=1&amp;format=rss&amp;max_auto_miles=100000&amp;max_price=10000&amp;postal=97006&amp;search_distance=50");
         ClientResponse clientResponse =webResource.get(ClientResponse.class);
-        System.out.println(clientResponse.getEntity(String.class));
+        String xmlData=clientResponse.getEntity(String.class);
+        //Todo: add handeler here to parse out the xml and give back the ArrayList
 }
 
     @When("^I go through all the pages of content$")
